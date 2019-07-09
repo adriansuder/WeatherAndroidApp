11a7bdad1f467d1a27ae475dd6d4f5a0c2e7317b
adriansuder <adrian.suder@microsoft.wsei.edu.pl>
2019-07-09 22:24:35 +0200

    pobieranie ikon z serwera, parsowanie jsona

--- WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
+++ WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
@@ -19,14 +19,17 @@ namespace WeatherAndroidApp
             httpClient = new HTTPClient();
         }
         
-
         async void OnButtonWeatherClicked(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(cityEntry.Text)) { 
-            var city = cityEntry.Text;
-            WeatherResult weatherResult = await httpClient.GetWeatherProperties(API_LINK + city + "&APPID=" + API_KEY);
-            BindingContext = weatherResult;
+            if (!string.IsNullOrWhiteSpace(cityEntry.Text))
+            { 
+                var city = cityEntry.Text;
+                WeatherResult weatherResult = await httpClient.GetWeatherProperties(API_LINK + city + "&APPID=" + API_KEY);
+                BindingContext = weatherResult;
+                WeatherIcon.Source = "http://openweathermap.org/img/w/" + weatherResult._weather[0]._icon + ".png";
+                test.Text = "http://openweathermap.org/img/w/" + weatherResult._weather[0]._icon + ".png";
             }
         }
+        
     }
 }
