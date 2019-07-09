ad5850274f1f612978c7466d822571227dcf675a
adriansuder <adrian.suder@microsoft.wsei.edu.pl>
2019-07-09 17:53:49 +0200

    test

---
+++ WeatherAndroidApp/WeatherAndroidApp/HTTPClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace WeatherAndroidApp
+{
+    public class HTTPClient
+    {
+        HttpClient httpClient;
+        public HTTPClient()
+        {
+            httpClient = new HttpClient();
+        }
+        public async Task<WeatherResult> GetWeatherProperties(string cityWeather)
+        {
+            WeatherResult weatherResult = null;
+            var temp = await httpClient.GetAsync(cityWeather);
+            if (temp.IsSuccessStatusCode)
+            {
+                var responseData = await temp.Content.ReadAsStringAsync();
+                weatherResult = JsonConvert.DeserializeObject<WeatherResult>(responseData);
+            }
+            return weatherResult;
+        }
+    }
+}
--- WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
+++ WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
@@ -9,9 +9,21 @@ namespace WeatherAndroidApp
 {
     public partial class MainPage : ContentPage
     {
+        public static string API_KEY = "26553c7563ad071303a5d9f9ed77222b";
+        public static string API_LINK = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+        HTTPClient httpClient;
         public MainPage()
         {
             InitializeComponent();
+            httpClient = new HTTPClient();
+        }
+
+        async void OnButtonWeatherClicked(object sender, EventArgs e)
+        {
+            var city = cityEntry.Text;
+            WeatherResult weatherResult = await httpClient.GetWeatherProperties(API_LINK + city + "&APPID=" + API_KEY);
+            BindingContext = weatherResult;
         }
     }
 }
