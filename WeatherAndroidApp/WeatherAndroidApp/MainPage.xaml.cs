1445616d49b4d0172d63d873cb9c4fe6faab8dcd
adriansuder <adrian.suder@microsoft.wsei.edu.pl>
2019-07-10 17:20:40 +0200

    UI ,api

--- WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
+++ WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
@@ -23,16 +23,14 @@ namespace WeatherAndroidApp
         async void OnButtonWeatherClicked(object sender, EventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(cityEntry.Text))
-            { 
+            {
                 var city = cityEntry.Text;
                 WeatherResult weatherResult = await httpClient.GetWeatherProperties(API_LINK + city + "&units=metric&APPID=" + API_KEY);
                 BindingContext = weatherResult;
                 WeatherIcon.Source = "http://openweathermap.org/img/wn/" + weatherResult._weather[0]._icon + "@2x.png";
-                //ForecastResult forecastResult = await httpClient.GetForecastProperties(API_FORECAST + city + "&units=metric&APPID=" + API_KEY);
-                //BindingContext = forecastResult;
-                //test.Text = API_FORECAST + city + "&units=metric&APPID=" + API_KEY;
+                //if (gridWeather.IsVisible == false) { gridWeather.IsVisible = true; }
             }
         }
-        
+    
     }
 }
