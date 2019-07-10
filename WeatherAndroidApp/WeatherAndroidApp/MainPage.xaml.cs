9db78d08f9182a3127f87bbea8d72010bbcf609a
adriansuder <adrian.suder@microsoft.wsei.edu.pl>
2019-07-10 14:42:11 +0200

    UI

--- WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
+++ WeatherAndroidApp/WeatherAndroidApp/MainPage.xaml.cs
@@ -27,9 +27,9 @@ namespace WeatherAndroidApp
                 var city = cityEntry.Text;
                 WeatherResult weatherResult = await httpClient.GetWeatherProperties(API_LINK + city + "&units=metric&APPID=" + API_KEY);
                 BindingContext = weatherResult;
-                ForecastResult forecastResult = await httpClient.GetForecastProperties(API_FORECAST + city + "&units=metric&APPID=" + API_KEY);
                 WeatherIcon.Source = "http://openweathermap.org/img/wn/" + weatherResult._weather[0]._icon + "@2x.png";
-                BindingContext = forecastResult;
+                //ForecastResult forecastResult = await httpClient.GetForecastProperties(API_FORECAST + city + "&units=metric&APPID=" + API_KEY);
+                //BindingContext = forecastResult;
                 //test.Text = API_FORECAST + city + "&units=metric&APPID=" + API_KEY;
             }
         }
